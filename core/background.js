d6acf93cc900c772f3ecd6c925eecd64a99e366a
Talha <talha_adeel@yahoo.com>
2023-02-26 19:17:17 -0500

    Init

---
+++ core/background.js
@@ -0,0 +1,17 @@
+/**
+ * Listens for the click event on the extension icon
+ */
+chrome.action.onClicked.addListener((tab) => {
+    fetch(chrome.runtime.getURL('../data.json'))
+	.then((response) => response.json())
+    .then((json) => openUrlInNewTab(json.url));
+});
+
+/**
+ * Opens a new tab with given URL
+ */
+function openUrlInNewTab(url){
+    chrome.tabs.create({
+        url: url
+    });
+}
---
+++ core/contentscript.js
@@ -0,0 +1,45 @@
+/**
+ * Document ready event
+ */
+$(function () {
+	fetch(chrome.runtime.getURL('../data.json'))
+	.then((response) => response.json())
+    .then((json) => injectFormData(json))
+	.then(() => focusCaptchaField());
+});
+
+/**
+ * Injects form data
+ */
+function injectFormData(data) {
+
+	// Last Name:
+	$("input#appointment_newAppointmentForm_lastname").val(data.last_name);
+	
+	// First Name:
+	$("input#appointment_newAppointmentForm_firstname").val(data.first_name);
+	
+	// Email:
+	$("input#appointment_newAppointmentForm_email").val(data.email);
+
+	// Repeat Email:
+	$("input#appointment_newAppointmentForm_emailrepeat").val(data.email);
+
+	// Passport number:
+	$("input#appointment_newAppointmentForm_fields_0__content").val(data.passport_number);
+
+	// Citizenship:
+	$("select#appointment_newAppointmentForm_fields_1__content").val(data.citizenship);
+
+	// Date of collection of fingerprints:
+	$("input#fields2content").val(data.fingerprint_collection_date);
+}
+
+/**
+ * Focuses the captcha field
+ */
+function focusCaptchaField(){
+	
+	// Captcha:
+	$("input#appointment_newAppointmentForm_captchaText").focus();
+}
